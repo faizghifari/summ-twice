d3acbfa64b19e4be06dce1a87c98905c0b3a6064
Faiz Ghifari Haznitrama <fgh1997@live.com>
2023-03-17 17:53:27 +0900

    modify tools in utils w scoring and cleaning func

--- utils/tools.py
+++ utils/tools.py
@@ -1,14 +1,35 @@
-import ssl
 import nltk
 
-def download_nltk():
-    try:
-        _create_unverified_https_context = ssl._create_unverified_context
-    except AttributeError:
-        pass
-    else:
-        ssl._create_default_https_context = _create_unverified_https_context
+nltk.download("punkt")
+nltk.download('stopwords')
+stopwords = set(stopwords.words('english'))
 
-    nltk.download("punkt")
+def get_score(text):
+    """
+    Remove stopwords from text, and compute score based on number of non-stopwords in text
+    
+    Args:
+    text: A string representing the text to be scored.
 
-download_nltk()
+    Returns:
+    A integer value representing the text's score.
+    """
+    text = ' '.join([word for word in text.split() if word not in stopwords])
+    score = len(text.split())
+    return score
+
+def clean_data(text: str) -> str:
+    """
+    Cleans the text by removing stopwords, and replacing special markers with whitespace.
+
+    Args:
+    text: A string representing the text to be cleaned.
+
+    Returns:
+    A string representing the cleaned text.
+    """
+    text = text.replace('{ disfmarker }', '')
+    text = text.replace('[ laughter . ]', ' ')
+    text = text.replace('[ inaudible . ] ', '')
+    text = ' '.join([w for w in text.split()])
+    return text
