f49cf2a475cbcfd63875c3b3d69536a5de60b58d
Faiz Ghifari Haznitrama <fgh1997@live.com>
2023-03-17 17:53:05 +0900

    add data loader class

---
+++ utils/dataloader.py
@@ -0,0 +1,31 @@
+import json
+
+class DataLoader:
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+
+    def load_data(self) -> Tuple[List[str], List[str]]:
+        """
+        Loads dataset from the data_path file.
+
+        Returns:
+        dataset: A list of meetings data from the data_path.
+        """
+        dataset = []
+        with open(self.data_path) as f:
+            queries = []
+            targets = []
+            utter = []
+            utter_speaker = []
+            data = json.load(f)
+
+            for query_type in ['general_query_list', 'specific_query_list']:
+                for q in data[query_type]:
+                    queries.append(q['query'])
+                    targets.append(q['answer'])
+            for turn in data['meeting_transcripts']:
+                content = clean_data(turn['content'])
+                utter.append(content)
+                utter_speaker.append(turn['speaker'] + ' : ' + content)
+
+        return dataset
