a02a18a6f9ee08a379ca5fa30f254b79bee85176
Faiz Ghifari Haznitrama <fgh1997@live.com>
2023-03-21 22:12:31 +0900

    add more args, add output

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import json
 import torch
 import argparse
 
-from topic.bertopic import BERTopicModel
+from tqdm import tqdm
 
+from topic.bertopic import BERTopicModel
+from topic.linearseg import LinearSegmenter
+
+from summarizer.evaluator import Evaluator
 from summarizer.model import IterativeSummarizer
 
 from utils.dataloader import DataLoader
@@ -13,23 +18,31 @@ device = 0 if torch.cuda.is_available() else -1
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, required=True)
+    parser.add_argument('--output_path', type=str, required=True)
     parser.add_argument('--model_name_or_path', type=str, required=True)
     parser.add_argument('--max_model_len', type=int, required=True)
     parser.add_argument('--max_seg_tgt_len', type=int, required=True)
     parser.add_argument('--max_tgt_len', type=int, required=True)
     parser.add_argument('--min_tgt_len', type=int, required=True)
+    parser.add_argument('--segmenter_type', type=str, default='bertopic')
     parser.add_argument('--doc_len_threshold', type=int, default=5)
+    parser.add_argument('--remove_noise', type=bool, default=True)
     return parser.parse_args()
 
-def main():
+def main(): 
     args = parse_args()
+    # add fixed random seed
     data_loader = DataLoader(args.data_path)
     dataset = data_loader.load_data()
 
     max_len_query = get_max_len_query(dataset)
-    max_length = args.max_model_len - max_len_query - args.max_seg_tgt_len
+    max_length = args.max_model_len - max_len_query - args.max_seg_tgt_len - 7
 
-    bertopic_model = BERTopicModel(max_length, args.doc_len_threshold)
+    if args.segmenter_type == 'bertopic':
+        segmenter = BERTopicModel(max_length, args.doc_len_threshold)
+    elif args.segmenter_type == 'linear':
+        segmenter = LinearSegmenter(max_length, args.doc_len_threshold)
+    
     summarizer = IterativeSummarizer(
         args.model_name_or_path, 
         args.max_model_len, 
@@ -38,13 +51,21 @@ def main():
         args.min_tgt_len,
         device=device)
 
-    for data in dataset:
-        topic_texts = bertopic_model.get_topics_text(data['utter'], data['utter_speaker'])
-        for query in data['queries']:
-            all_summaries = summarizer.summarize_texts(topic_texts, query)
-            final_summary = all_summaries[-1]
-            
-    # continue with the rest of your code
+    targets = []
+    all_summaries = []
+    for data in tqdm(dataset):
+        segment_texts = segmenter.segmentize(data, remove_noise=args.remove_noise)
+        for i in range(len(data['queries'])):
+            all_summaries.append({
+                "all_summaries": summarizer.summarize_texts(segment_texts, data['queries'][i])
+            })
+            targets.append(data['targets'][i])
+    
+    with open(args.output_path, "w") as f:
+        for summaries in all_summaries:
+            print(json.dumps(summaries), file=f)
+    with open('./data/qmsum/target.txt', 'w') as f:
+        f.writelines(targets)
 
 if __name__ == '__main__':
     main()
