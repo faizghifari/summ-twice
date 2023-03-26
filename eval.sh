bbb99c1b053bde4b92ad6335c9228e3c75458e48
Faiz Ghifari Haznitrama <fgh1997@live.com>
2023-03-26 11:56:48 +0900

    add script to evaluate summary results

---
+++ eval.sh
@@ -0,0 +1,4 @@
+python evaluate.py \
+    --pred_path ./output/test-pegasus_large-contrastive-bertopic.json \
+    --target_path ./data/qmsum/target.txt \
+    --results_path ./results/test-pegasus_large-contrastive-bertopic.json
